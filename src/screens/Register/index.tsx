a67341600443e77b79441d2f524d32e4e511fd9f
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-06-07 16:01:57 -0300

    feat: add transaction types

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "../../components/Form/Input";
+import { Button } from "../../components/Form/Button";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 
-import { Container, Header, Title, Form } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionsTypes,
+} from "./styles";
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("");
+
+  function handleTransactionsTypeSelect(type: "up" | "down") {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -12,8 +28,29 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Preço" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+
+          <TransactionsTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionsTypeSelect("up")}
+              isActive={transactionType === "up"}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionsTypeSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
+        </Fields>
+
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
