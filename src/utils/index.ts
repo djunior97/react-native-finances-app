c9903bc476f2ffd4f7bbb3d0cb8657d88343f168
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-06-08 13:35:21 -0300

    feat: create get data from storage util function

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+async function getData(key: string) {
+  const data = await AsyncStorage.getItem(key);
+
+  return data ? JSON.parse(data!) : data;
+}
+
+export const storage = {
+  getData,
+};
