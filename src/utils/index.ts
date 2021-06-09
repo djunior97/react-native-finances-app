b09cd8e27123fd40db5c18e82d78daf12ae85c4f
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-06-09 11:49:12 -0300

    refactor: remove unnecessary code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 async function getData(key: string) {
   const data = await AsyncStorage.getItem(key);
 
-  return data ? JSON.parse(data!) : data;
+  return data ? JSON.parse(data) : data;
 }
 
 export const storage = {
