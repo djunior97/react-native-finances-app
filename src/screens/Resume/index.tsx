98764c2337ecf26f12adf6d20ada48b76ff0a707
Dirceu Jr <dirceu.junior@ntconsultcorp.com>
2021-06-12 13:03:28 -0300

    feat: add filter by month

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { VictoryPie } from "victory-native";
 import { useTheme } from "styled-components";
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useFocusEffect } from "@react-navigation/native";
+import { addMonths, subMonths, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { HistoryCard } from "../../components/HistoryCard";
 
@@ -17,6 +20,11 @@ import {
   Content,
   ChartContainer,
   HistoryCardList,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+  LoadContainer,
 } from "./styles";
 
 export interface CategoryData {
@@ -30,20 +38,35 @@ export interface CategoryData {
 }
 
 export function Resume() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>(
     []
   );
 
   const theme = useTheme();
 
+  function handleChangeDate(action: "prev" | "next") {
+    setSelectedDate(
+      action === "prev"
+        ? subMonths(selectedDate, 1)
+        : addMonths(selectedDate, 1)
+    );
+  }
+
   async function loadData() {
+    setIsLoading(true);
+
     const dataKey = "@gofinances:transactions";
 
     const response = await storage.getData(dataKey);
     const transactions = response ? response : [];
 
     const expenses = transactions.filter(
-      (expense: TransactionProps) => expense.type === "negative"
+      (expense: TransactionProps) =>
+        expense.type === "negative" &&
+        new Date(expense.date).getMonth() === selectedDate.getMonth() &&
+        new Date(expense.date).getFullYear() === selectedDate.getFullYear()
     );
 
     const expensesTotal = expenses.reduce(
@@ -86,11 +109,16 @@ export function Resume() {
     });
 
     setTotalByCategories(totalByCategory);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
   }
 
-  useEffect(() => {
-    loadData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [selectedDate])
+  );
 
   return (
     <Container>
@@ -98,25 +126,44 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <Content>
-        <ChartContainer>
-          <VictoryPie
-            data={totalByCategories}
-            x="formattedPercent"
-            y="total"
-            colorScale={totalByCategories.map((category) => category.color)}
-            style={{
-              labels: {
-                fontSize: RFValue(18),
-                fontWeight: "bold",
-                fill: theme.colors.shape,
-              },
-            }}
-            labelRadius={75}
-          />
-        </ChartContainer>
+      {isLoading ? (
+        <LoadContainer>
+          <ActivityIndicator color={theme.colors.secondary} size="large" />
+        </LoadContainer>
+      ) : (
+        <Content>
+          <MonthSelect>
+            <MonthSelectButton onPress={() => handleChangeDate("prev")}>
+              <MonthSelectIcon name="chevron-left" />
+            </MonthSelectButton>
 
-        {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+            <Month>
+              {format(selectedDate, "MMMM, yyyy", { locale: ptBR })}
+            </Month>
+
+            <MonthSelectButton onPress={() => handleChangeDate("next")}>
+              <MonthSelectIcon name="chevron-right" />
+            </MonthSelectButton>
+          </MonthSelect>
+
+          <ChartContainer>
+            <VictoryPie
+              data={totalByCategories}
+              x="formattedPercent"
+              y="total"
+              colorScale={totalByCategories.map((category) => category.color)}
+              style={{
+                labels: {
+                  fontSize: RFValue(18),
+                  fontWeight: "bold",
+                  fill: theme.colors.shape,
+                },
+              }}
+              labelRadius={75}
+            />
+          </ChartContainer>
+
+          {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
           <HistoryCard
             key="{item.key}"
             title="{item.name}"
@@ -124,19 +171,20 @@ export function Resume() {
             color="{item.color}"
           />
         ))} */}
-        <HistoryCardList
-          data={totalByCategories}
-          keyExtractor={(item) => item.key}
-          renderItem={({ item }) => (
-            <HistoryCard
-              key={item.key}
-              title={item.name}
-              amount={item.formattedTotal}
-              color={item.color}
-            />
-          )}
-        />
-      </Content>
+          <HistoryCardList
+            data={totalByCategories}
+            keyExtractor={(item) => item.key}
+            renderItem={({ item }) => (
+              <HistoryCard
+                key={item.key}
+                title={item.name}
+                amount={item.formattedTotal}
+                color={item.color}
+              />
+            )}
+          />
+        </Content>
+      )}
     </Container>
   );
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { FlatList } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
 import { CategoryData } from ".";
 
@@ -42,4 +44,31 @@ export const HistoryCardList = styled(
   showsVerticalScrollIndicator: false,
 })`
   width: 100%;
+`;
+
+export const MonthSelect = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 24px;
+`;
+
+export const MonthSelectButton = styled(BorderlessButton)``;
+
+export const MonthSelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+`;
+
+export const Month = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+`;
+
+export const LoadContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
